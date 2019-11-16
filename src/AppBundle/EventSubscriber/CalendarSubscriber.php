10b606dba761fc7eedc5c2aca6f27587ab8c2bee
Tleard <huxtra@gmail.com>
2019-11-16 16:18:18 +0100

    Working Callendar Bundle Fc load Ajax

--- src/AppBundle/EventSubscriber/CalendarSubscriber.php
+++ src/AppBundle/EventSubscriber/CalendarSubscriber.php
@@ -78,4 +78,47 @@ class CalendarSubscriber implements EventSubscriberInterface
             $calendar->addEvent($bookingEvent);
         }
     }
+
+    public function load(CalendarEvent $calendar): void
+    {
+        $start = $calendar->getStart();
+        $end = $calendar->getEnd();
+        $filters = $calendar->getFilters();
+
+        // Modify the query to fit to your entity and needs
+        // Change booking.beginAt by your start date property
+        $bookings = $this->bookingRepository
+            ->createQueryBuilder('booking')
+            ->where('booking.beginAt BETWEEN :start and :end')
+            ->setParameter('start', $start->format('Y-m-d H:i:s'))
+            ->setParameter('end', $end->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->getResult()
+        ;
+
+        foreach ($bookings as $booking) {
+            // this create the events with your data (here booking data) to fill calendar
+            $bookingEvent = new Event(
+                $booking->getTitle(),
+                $booking->getBeginAt(),
+                $booking->getEndAt() // If the end date is null or not defined, a all day event is created.
+            );
+
+            /*
+             * Add custom options to events
+             *
+             * For more information see: https://fullcalendar.io/docs/event-object
+             * and: https://github.com/fullcalendar/fullcalendar/blob/master/src/core/options.ts
+             */
+
+            $bookingEvent->setOptions([
+                'backgroundColor' => 'red',
+                'borderColor' => 'red',
+            ]);
+            $bookingEvent->addOption('url', 'https://github.com');
+
+            // finally, add the event to the CalendarEvent to fill the calendar
+            $calendar->addEvent($bookingEvent);
+        }
+    }
 }
