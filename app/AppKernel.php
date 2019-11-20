a11dbd79feef380400fd1f35f5d3e37b78076e0e
Tleard <huxtra@gmail.com>
2019-11-20 21:59:56 +0100

    Knp Snappy PDF Install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new CalendarBundle\CalendarBundle()
+            new CalendarBundle\CalendarBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
